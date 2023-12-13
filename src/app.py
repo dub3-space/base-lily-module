47e433c4433246810b96d8f5da1d48de1e4f8252
Fabriziogianni7 <fabrizio@keyko.io>
2023-12-13 11:55:12 +0100

    update to app.py

--- src/app.py
+++ src/app.py
@@ -3,16 +3,16 @@ import sys
 
 def main():
     
-    directory = '/outputs'
-    os.makedirs(directory, exist_ok=True)
+    # directory = '/outputs'
+    # os.makedirs(directory, exist_ok=True)
 
-    with open(os.path.join(directory, 'example.txt'), 'a') as file:
-        file.write('Hola, la jente esta muy loca tio!')
+    # with open(os.path.join(directory, 'example.txt'), 'a') as file:
+    #     file.write('Hola, la jente esta muy loca tio!')
 
     print("just wrote some shit here broo")
 
 
 if __name__ == "__main__":
-    print("just wrote some shit here broo from main")
+    # print("just wrote some shit here broo from main")
 
     main()
